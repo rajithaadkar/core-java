6c1303eb31f32088e862816eb918de93f860bd8f
rajithaadkar <rajithaadkar0@gmail.com>
2026-03-16 10:05:07 +0530

    Switch case example

---
+++ switch case/ContactsRunner.java
@@ -0,0 +1,236 @@
+class ContactsRunner{
+	
+	public static void main(String[] args) {
+
+ 
+	long number = Contacts.getContactNoByName("Ajay");
+        System.out.println("Phone Number: " + number);
+		
+		 number = Contacts.getContactNoByName("Ajith");
+        System.out.println("Phone Number: " + number);
+		
+		 number = Contacts.getContactNoByName("Akhila Kvg ");
+        System.out.println("Phone Number: " + number);
+		
+		number = Contacts.getContactNoByName("Amith Anna");
+        System.out.println("Phone Number: " + number);
+		
+		number = Contacts.getContactNoByName("Amma");
+        System.out.println("Phone Number: " + number);
+		
+		number = Contacts.getContactNoByName("Bhoomi");
+        System.out.println("Phone Number: " + number);
+		
+		number = Contacts.getContactNoByName("Chethan");
+        System.out.println("Phone Number: " + number);
+		
+		 number = Contacts.getContactNoByName("Janani");
+        System.out.println("Phone Number: " + number);
+
+        number = Contacts.getContactNoByName("Anjali");
+        System.out.println("Phone Number: " + number);
+
+        number = Contacts.getContactNoByName("Ravi");
+        System.out.println("Phone Number: " + number);
+
+        number = Contacts.getContactNoByName("Suresh");
+        System.out.println("Phone Number: " + number);
+
+        number = Contacts.getContactNoByName("Priya");
+        System.out.println("Phone Number: " + number);
+
+        number = Contacts.getContactNoByName("Riya");
+		System.out.println("Phone Number: " + number);
+
+		number = Contacts.getContactNoByName("Sneha");
+		System.out.println("Phone Number: " + number);
+
+		number = Contacts.getContactNoByName("Pooja");
+		System.out.println("Phone Number: " + number);
+
+		number = Contacts.getContactNoByName("Kavya");
+		System.out.println("Phone Number: " + number);
+
+		number = Contacts.getContactNoByName("Divya");
+		System.out.println("Phone Number: " + number);
+
+		number = Contacts.getContactNoByName("Nisha");
+		System.out.println("Phone Number: " + number);
+
+		number = Contacts.getContactNoByName("Asha");
+		System.out.println("Phone Number: " + number);
+
+		number = Contacts.getContactNoByName("Deepa");
+		System.out.println("Phone Number: " + number);
+
+		number = Contacts.getContactNoByName("Neha");
+		System.out.println("Phone Number: " + number);
+
+		number = Contacts.getContactNoByName("Priya");
+		System.out.println("Phone Number: " + number);
+
+		number = Contacts.getContactNoByName("Lakshmi");
+		System.out.println("Phone Number: " + number);
+
+		number = Contacts.getContactNoByName("Radha");
+		System.out.println("Phone Number: " + number);
+
+		number = Contacts.getContactNoByName("Shreya");
+		System.out.println("Phone Number: " + number);
+
+		number = Contacts.getContactNoByName("Anu");
+		System.out.println("Phone Number: " + number);
+
+		number = Contacts.getContactNoByName("Swathi");
+		System.out.println("Phone Number: " + number);
+
+		number = Contacts.getContactNoByName("Pavitra");
+		System.out.println("Phone Number: " + number);
+
+		number = Contacts.getContactNoByName("Bhavya");
+		System.out.println("Phone Number: " + number);
+
+		number = Contacts.getContactNoByName("Sanjana");
+		System.out.println("Phone Number: " + number);
+
+		number = Contacts.getContactNoByName("Rashmi");
+		System.out.println("Phone Number: " + number);
+
+		number = Contacts.getContactNoByName("Shilpa");
+		System.out.println("Phone Number: " + number);
+
+		number = Contacts.getContactNoByName("Harini");
+		System.out.println("Phone Number: " + number);
+
+		number = Contacts.getContactNoByName("Nandini");
+		System.out.println("Phone Number: " + number);
+
+		number = Contacts.getContactNoByName("Manasa");
+		System.out.println("Phone Number: " + number);
+
+		number = Contacts.getContactNoByName("Varsha");
+		System.out.println("Phone Number: " + number);
+
+		number = Contacts.getContactNoByName("Chandana");
+		System.out.println("Phone Number: " + number);
+
+		number = Contacts.getContactNoByName("Soumya");
+		System.out.println("Phone Number: " + number);
+
+		number = Contacts.getContactNoByName("Pallavi");
+		System.out.println("Phone Number: " + number);
+
+		number = Contacts.getContactNoByName("Ritu");
+		System.out.println("Phone Number: " + number);
+
+		number = Contacts.getContactNoByName("Preeti");
+		System.out.println("Phone Number: " + number);
+
+		number = Contacts.getContactNoByName("Jyothi");
+		System.out.println("Phone Number: " + number);
+
+		number = Contacts.getContactNoByName("Kiran");
+		System.out.println("Phone Number: " + number);
+
+		number = Contacts.getContactNoByName("Madhu");
+		System.out.println("Phone Number: " + number);
+
+		number = Contacts.getContactNoByName("Geetha");
+		System.out.println("Phone Number: " + number);
+
+		number = Contacts.getContactNoByName("Suma");
+		System.out.println("Phone Number: " + number);
+
+		number = Contacts.getContactNoByName("Sandhya");
+		System.out.println("Phone Number: " + number);
+
+		number = Contacts.getContactNoByName("Archana");
+		System.out.println("Phone Number: " + number);
+
+		number = Contacts.getContactNoByName("Keerthi");
+		System.out.println("Phone Number: " + number);
+
+		number = Contacts.getContactNoByName("Tejaswini");
+		System.out.println("Phone Number: " + number);
+
+		number = Contacts.getContactNoByName("Shruthi");
+		System.out.println("Phone Number: " + number);
+
+		number = Contacts.getContactNoByName("Aishwarya");
+		System.out.println("Phone Number: " + number);
+
+		number = Contacts.getContactNoByName("Megha");
+		System.out.println("Phone Number: " + number);
+
+		number = Contacts.getContactNoByName("Pavani");
+		System.out.println("Phone Number: " + number);
+
+		number = Contacts.getContactNoByName("Navya");
+		System.out.println("Phone Number: " + number);
+
+		number = Contacts.getContactNoByName("Radhika");
+		System.out.println("Phone Number: " + number);
+
+		number = Contacts.getContactNoByName("Sahana");
+		System.out.println("Phone Number: " + number);
+
+		number = Contacts.getContactNoByName("Tanuja");
+		System.out.println("Phone Number: " + number);
+
+		number = Contacts.getContactNoByName("Yashika");
+		System.out.println("Phone Number: " + number);
+
+		number = Contacts.getContactNoByName("Ishita");
+		System.out.println("Phone Number: " + number);
+
+		number = Contacts.getContactNoByName("Komal");
+		System.out.println("Phone Number: " + number);
+
+		number = Contacts.getContactNoByName("Latha");
+		System.out.println("Phone Number: " + number);
+		
+		number = Contacts.getContactNoByName("Vikas");
+		System.out.println("Phone Number: " + number);
+		
+		number = Contacts.getContactNoByName("Yogesh");
+		System.out.println("Phone Number: " + number);
+		
+		number = Contacts.getContactNoByName("Zubair");
+		System.out.println("Phone Number: " + number);
+		
+		number = Contacts.getContactNoByName("Aditya");
+		System.out.println("Phone Number: " + number);
+		
+		number = Contacts.getContactNoByName("Bhaskar");
+		System.out.println("Phone Number: " + number);
+		
+		number = Contacts.getContactNoByName("Chandan");
+		System.out.println("Phone Number: " + number);
+		
+		number = Contacts.getContactNoByName("Dinesh");
+		System.out.println("Phone Number: " + number);
+		
+		
+		number = Contacts.getContactNoByName("Eshwar");
+		System.out.println("Phone Number: " + number);
+		
+		number = Contacts.getContactNoByName("suma");
+		System.out.println("Phone Number: " + number);
+		 
+		 number = Contacts.getContactNoByName("Arpita");
+		System.out.println("Phone Number: " + number);
+
+		number = Contacts.getContactNoByName("Gopal");
+		System.out.println("Phone Number: " + number);
+
+		number = Contacts.getContactNoByName("Hitesh");
+		System.out.println("Phone Number: " + number);
+
+		number = Contacts.getContactNoByName("Maya");
+		System.out.println("Phone Number: " + number);
+
+		
+		
+
+	}
+}
